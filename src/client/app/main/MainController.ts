6f0ef5a8b5c96eec9f9338b240020d3d0b149bd8
gvasko <gabor.vaskoszedlar@gmail.com>
2018-05-13 19:35:59 +0000

    Start page done

--- src/client/app/main/MainController.ts
+++ src/client/app/main/MainController.ts
@@ -2,8 +2,23 @@
 
 module Main {
     export class MainController {
+        constructor(private $scope: any) {
+
+        }
+
+        createOpenDocumentDialog() {
+            console.log("Open");
+        }
+
+        createSaveDocumentDialog() {
+            console.log("Save");
+        }
+
+        createDeleteDocumentDialog() {
+            console.log("Delete");
+        }
 
     }
 }
 
-
+angular.module("app").controller("MainController", ["$scope", Main.MainController]);
